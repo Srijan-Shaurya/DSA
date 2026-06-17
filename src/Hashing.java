53415d22153722ec69ed21f8663817f82efb2528
Srijan Shaurya <i.srijan.shaurya@gmail.com>
2026-06-17 07:18:55 +0530

    basic changes

--- src/Hashing.java
+++ src/Hashing.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class Hashing {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter The Length the array");
         int n = input.nextInt();
+        System.out.println("Now Enter The Array");
         int[] arr = new int[n];
         for (int i = 0;i<n;i++){
             arr[i] = input.nextInt();
@@ -13,7 +15,7 @@ public class Hashing {
         for (int i = 0;i<n;i++){
             hash[arr[i]]+=1;
         }
-        System.out.println(hash[2]);
+        System.out.println("Number Of Times 2 Appears in array is "+hash[2]);
 
     }
 }
